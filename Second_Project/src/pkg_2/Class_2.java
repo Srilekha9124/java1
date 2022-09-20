3aef704d87e569be1fb3a40cbce3b8d38856a6f4
Srilekha9124 <113813435+Srilekha9124@users.noreply.github.com>
2022-09-20 16:05:02 +0530

    Update Class_2.java

--- Second_Project/src/pkg_2/Class_2.java
+++ Second_Project/src/pkg_2/Class_2.java
@@ -13,8 +13,8 @@ public class Class_2
 		//System.out.println("maximum value of short"+ Short.MAX_VALUE);
 		int c = a+b;
 		System.out. println(c);
-		System.out. println(b);
-		System.out. println(b);
+		
+		
 
 		
 		short x = 50;
